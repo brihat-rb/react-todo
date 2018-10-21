a8804d93daf09f14369e9e51a91a72627694ac6e
brihat-rb <brihat.rb@gmail.com>
2018-10-21 17:47:15 +0545

    Updates

---
+++ precache-manifest.29f43e3aeef3b60488f41f0e51600e01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e7de01cfd73dbb55414",
+    "url": "/static/js/main.0e7de01c.chunk.js"
+  },
+  {
+    "revision": "e7eac33005b228a0b861",
+    "url": "/static/js/1.e7eac330.chunk.js"
+  },
+  {
+    "revision": "0e7de01cfd73dbb55414",
+    "url": "/static/css/main.7c2bc5b3.chunk.css"
+  },
+  {
+    "revision": "548620e044f3eeb22e5b79a02c9ff0f0",
+    "url": "/index.html"
+  }
+];
